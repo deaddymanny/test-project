588b087ef9252107981c66f554ebe3d282b491ef
Poruch <139491864+Poruch@users.noreply.github.com>
2025-06-15 18:54:29 +0500

    add basic layout and login page

--- app.vue
+++ app.vue
@@ -1,13 +1,6 @@
 <template> 
 <NuxtLayout >
-  <MyComponent />
-  <div class="main-container">
     <NuxtPage />
-  </div>
   </NuxtLayout>
 </template>
-<style scoped>
-.main-container{
-  background-color: aqua;
-}
-</style>
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup lang = "ts">
+import { ClientOnly } from '#components';
+
+    let drawer = ref<boolean>(false);
+</script>
 <template>
-    <h2>header</h2>
-    <v-app>
-        <slot />
-    </v-app>
+    <v-responsive>
+        <v-app>
+<v-app-bar title="Название сайта">
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </v-app-bar>
+        
+            <ClientOnly>
+        <v-navigation-drawer v-model = "drawer" location="right">
+            <v-list nav>
+                <v-list-item title = "Главная" to = "/" link></v-list-item>    
+                <v-list-item title = "Кабинет" to = "/cabinet" link></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        </ClientOnly>
+
+        <v-main>
+            <v-container>
+                <slot></slot>
+            </v-container>
+        </v-main>
+        </v-app>
+    </v-responsive>
 </template>
