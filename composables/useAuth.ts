ddee2e92de94da3129bc28346d4abea1fba33068
Jet-Green <grishadzyin@gmail.com>
2025-06-29 19:40:11 +0500

    add registration

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -8,15 +8,27 @@ export const useAuth = () => {
     }
   })
 
-
   function login(email: string, password: string) {
     user.value.email = email;
     user.value.password = password;
   }
 
 
+  function registration(email: string, password: string) {
+    $fetch("http://localhost:3444/users/create",
+      {
+        method: "POST",
+        body: {
+          email,
+          password
+        } // { ...user.value } === user.value
+      }
+    )
+  }
+
   return {
     user: readonly(user),
     login,
+    registration,
   }
 }
---
+++ pages/registration.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { registration, user } = useAuth();
+const router = useRouter()
+
+let email = ref<string>("");
+let password = ref<string>("");
+
+function submit() {
+  registration(email.value, password.value);
+  router.push("/cabinet")
+}
+</script>
+<template>
+  <v-row class="d-flex justify-center">
+    <v-col cols="4">
+      <v-text-field v-model="email" label="email" type="email"></v-text-field>
+      <v-text-field v-model="password" label="Пароль" type="password"></v-text-field>
+
+      <v-btn @click="submit">регистрация</v-btn>
+    </v-col>
+  </v-row>
+</template>
